5b5339683cba02fcf1a373e86fca1feab624b849
RiyaChand-13 <riyat1320@gmail.com>
2026-04-09 00:57:11 +0530

    Added procfile and made some changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
-python ZenZest/manage.py collectstatic --no-input
-python ZenZest/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
